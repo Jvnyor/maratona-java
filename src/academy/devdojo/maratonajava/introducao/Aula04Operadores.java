a21208570bf0c7d88b9b000d877e2f26fedcae82
Josias Junior <josias100junior@gmail.com>
2021-12-04 01:00:32 -0300

    20 - Estruturas Condicionais pt 01 - IF 3:52 / 12:00

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -35,5 +35,32 @@ public class Aula04Operadores {
 		boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
 		System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaLeiMaiorQueTrinta);
 		System.out.println("isDentroDaLeiMenorQueTrinta "+isDentroDaLeiMenorQueTrinta);
+		
+		double valorTotalContaCorrente = 200;
+		double valorTotalContaPoupanca = 10000;
+		float valorPlaystation = 5000F;
+		boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+		
+		System.out.println("isPlaystationCincoCompravel "+isPlaystationCincoCompravel);
+		
+		// = += -= *= /= %=
+		
+		double bonus = 1800;
+		bonus += 1000; // 2800
+		bonus -= 1000; // 1800
+		bonus *=2; // 3600
+		bonus /=2;
+		bonus %=2;
+		System.out.println(bonus);
+		
+		// ++ --
+		int contador = 0;
+		contador += 1; // contador = contador + 1;
+		contador++;
+		contador--;
+		++contador;
+		--contador;
+		int contador2 = 0;
+		System.out.println(++contador2);
 	}
 }
---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+
+	public static void main(String[] args) {
+		if(true) {
+			System.out.println("Dentro do if");
+		}
+		System.out.println("Fora do if");
+	}
+}
