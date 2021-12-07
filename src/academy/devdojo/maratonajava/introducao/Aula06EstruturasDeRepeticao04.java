8d8eed93c34686e796a6cfcddeecd635f627f94b
Jvnyor <josias100junior@gmail.com>
2021-12-07 12:15:34 -0300

    32 - Arrays pt 01 0:00

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
@@ -1,11 +1,24 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula06EstruturasDeRepeticao04 {
-	// Dado um valor de um carro, descubra em quantas vezes ele pode ser parcelado
+	
 	public static void main(String[] args) {
+		// Solução do exercício minha
 		double valorCarro = 50000;
-		for(int i=1;i<=12;i++) {
+		double valorMinimoParcela = 1000;
+		for(int i=1;i<=(valorCarro / valorMinimoParcela);i++) {
 			System.out.println("O valor do carro de "+valorCarro+" pode ser parcelado em "+i+"x no valor de "+valorCarro / i);
+		}
+		
+		// Dado um valor de um carro, descubra em quantas vezes ele pode ser parcelado
+		// Condição valorParcela >= 1000
+		double valorTotal = 30000;
+		for(int parcela = 1;parcela <= valorTotal;parcela++) {
+			double valorParcela = valorTotal / parcela;
+			if (valorParcela < 1000) {
+				break;
+			}
+			System.out.println("Parcela "+parcela+" de R$ "+valorParcela);
 		}
 	}
 }
