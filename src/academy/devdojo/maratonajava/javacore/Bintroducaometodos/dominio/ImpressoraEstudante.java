dddc287c21610c3339184665fd3fb0e88ddfd076
Josias Junior <josias100junior@gmail.com>
2021-12-14 23:54:25 -0300

    51 - Orientação Objetos - Métodos pt 08 - Referência this 0:00

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
@@ -4,8 +4,10 @@ public class ImpressoraEstudante {
 
 	public void imprime(Estudante estudante) {
 		System.out.println("--------------");
+		
 		System.out.println(estudante.nome);
 		System.out.println(estudante.idade);
 		System.out.println(estudante.sexo);
+		estudante.nome="Gohan";
 	}
 }
