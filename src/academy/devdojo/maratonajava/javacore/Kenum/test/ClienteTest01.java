11870393c72fb52ec216fae333076bee61f9595c
Jvnyor <josias100junior@gmail.com>
2021-12-21 15:40:30 -0300

    80 - Orientação Objetos - Enumeração pt 01 - Introdução 3:00

---
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Kenum.test;
+
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
+
+public class ClienteTest01 {
+	public static void main(String[] args) {
+		Cliente cliente1 = new Cliente("Tsubasa","PESSOA_FISICA");
+		Cliente cliente2 = new Cliente("Tsubasa","PESSOA_JURIDICA");
+	}
+}
