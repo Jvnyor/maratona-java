3efb3dd661fe73e3e79e82fd08a2e52c272dbafd
Josias Junior <josias100junior@gmail.com>
2022-01-30 22:56:29 -0300

    106 - Classes Utilitárias - Wrappers pt 01 0:00

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/dominio/Leitor2.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.dominio;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class Leitor2 implements Closeable {
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Fechando leitor 2");
+	}
+
+}
