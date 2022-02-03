32b5b05afa74891c76b1236b6235d5c301884ae9
Josias Junior <josias100junior@gmail.com>
2022-02-02 23:15:27 -0300

    108 - Classes Utilitárias - Strings pt 01 1:44

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		String nome = "William"; //String constant pool
+	}
+}
