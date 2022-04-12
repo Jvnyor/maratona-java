552d708e2ba8a164c5fdb574348de5cb554ed681
Jvnyor <josias100junior@gmail.com>
2022-04-11 22:30:53 -0300

    143 - Classes Utilitárias - IO pt 06 - File para diretórios 0:00

---
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/FileWriterTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+// File
+// FileWriter
+// FileReader
+// BufferedWriter
+// BufferedReader
+public class FileWriterTest01 {
+	public static void main(String[] args) {
+		File file = new File("file.txt");
+		try (FileWriter fw = new FileWriter(file, true)) {
+			fw.write("O Devdojo é lindo, é o melhor curso do Brasillllll\nContinuando a cantoria na próxima linha\n");
+			fw.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
