a79f62210c55340e403211621b6392cb02882b11
Jvnyor <josias100junior@gmail.com>
2022-04-19 15:38:44 -0300

    152 - Classes Utilitárias - NIO pt 09 - PosixFileAttributes 3:45

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/PosixFileAttributesTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.nio.file.Paths;
+
+public class PosixFileAttributesTest01 {
+
+	public static void main(String[] args) {
+		Paths.get("/home/william/dev/file.txt");
+	}
+
+}
