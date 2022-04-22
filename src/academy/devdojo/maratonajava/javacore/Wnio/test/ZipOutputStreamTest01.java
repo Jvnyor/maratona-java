7b3b654f7ddf407a6fba471e7129f6412dcf146a
Jvnyor <josias100junior@gmail.com>
2022-04-22 15:52:40 -0300

    163 - Coleções pt 03 - hashCode pt 01 0:00

--- src/academy/devdojo/maratonajava/javacore/Wnio/test/ZipOutputStreamTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/ZipOutputStreamTest01.java
@@ -5,6 +5,7 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class ZipOutputStreamTest01 {
@@ -16,10 +17,14 @@ public class ZipOutputStreamTest01 {
 
 	private static void zip(Path arquivoZip, Path arquivosParaZipar) {
 		try (ZipOutputStream zipStream = new ZipOutputStream(Files.newOutputStream(arquivoZip));
-			 DirectoryStream<Path> directoryStream = Files.newDirectoryStream(arquivosParaZipar)) {
+				DirectoryStream<Path> directoryStream = Files.newDirectoryStream(arquivosParaZipar)) {
 			for (Path file : directoryStream) {
-				System.out.println(file.getFileName());
+				ZipEntry zipEntry = new ZipEntry(file.getFileName().toString());
+				zipStream.putNextEntry(zipEntry);
+				Files.copy(file, zipStream);
+				zipStream.closeEntry();
 			}
+			System.out.println("Arquivo criado com sucesso");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
