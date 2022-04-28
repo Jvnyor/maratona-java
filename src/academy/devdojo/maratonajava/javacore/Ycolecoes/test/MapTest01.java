16e06fcf16bc2a11d2c2779f9b17daf31338ca6f
Jvnyor <josias100junior@gmail.com>
2022-04-28 15:45:00 -0300

    183 - Generics pt 01 - Introdução 0:00

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
@@ -1,13 +1,12 @@
 package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MapTest01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Map<String, String> map = new HashMap<>();
+		Map<String, String> map = new LinkedHashMap<>();
 		map.put("teklado", "teclado");
 		map.put("mouze", "mouse");
 		map.put("vc", "você");
@@ -15,6 +14,15 @@ public class MapTest01 {
 		System.out.println(map);
 		for (String key : map.keySet()) {
 			System.out.println(key + " : " + map.get(key));
+		}
+		System.out.println("-------------");
+
+		for (String value : map.values()) {
+			System.out.println(value);
+		}
+		System.out.println("-------------");
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + " - " + entry.getValue());
 		}
 	}
 
