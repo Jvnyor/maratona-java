281f1071322c8036f0cf19553f7ab693f42ba0dd
Jvnyor <josias100junior@gmail.com>
2022-04-29 15:49:37 -0300

    188 - Generics pt 06 - Métodos Genéricos 0:00

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/dominio/Barco.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.dominio;
+
+public class Barco {
+	private String nome;
+
+	public Barco(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Barco [nome=" + nome + "]";
+	}
+}
