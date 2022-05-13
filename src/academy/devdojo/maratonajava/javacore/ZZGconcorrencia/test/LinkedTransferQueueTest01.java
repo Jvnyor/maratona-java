34a0f4735b9863fd5d3c6e4a972ec5a3eeaac649
Jvnyor <josias100junior@gmail.com>
2022-05-13 15:53:22 -0300

    235 - Concorrência pt 07 - LinkedTransferQueue 1:08

---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/LinkedTransferQueueTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+public class LinkedTransferQueueTest01 {
+
+	public static void main(String[] args) {
+//		ConcurrentLinkedQueue, SynchronousQueue, LinkedBlockingQueue
+		
+	}
+
+}
