ff824affe7e695f000dd09cedad02d81c221dce0
Jvnyor <josias100junior@gmail.com>
2022-05-22 12:00:21 -0300

    257 - JDBC pt 06 - Inserindo dados com Statement 1:09

---
+++ src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
+
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(ConnectionFactory.getConnection());
+	}
+
+}
