6396b0f447ed24db8afc6be9ec2c0265e3cda7b5
Jvnyor <josias100junior@gmail.com>
2022-05-24 10:59:24 -0300

    -

--- src/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ src/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -6,16 +6,15 @@ import java.sql.Statement;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
-import lombok.extern.log4j.Log4j2;
 
-@Log4j2
+//@Log4j2
 public class ProducerRepository {
 	public static void save(Producer producer) {
 		String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES ('%s');".formatted(producer.getName());
 		try (Connection conn = ConnectionFactory.getConnection();
 			 Statement stmt = conn.createStatement();) {
 			int rowsAffected = stmt.executeUpdate(sql);
-			log.info("Database rows affected {}", rowsAffected);
+//			log.info("Database rows affected {}", rowsAffected);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
