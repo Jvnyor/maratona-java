1215f9bdb7f11c98fa1f662eb57a8a83c66a46e5
Jvnyor <josias100junior@gmail.com>
2022-05-26 15:50:30 -0300

    264 - JDBC pt 13 - DatabaseMetaData 0:00

---
+++ src/academy/devdojo/maratonajava/javacore/ZZIjdbc/service/ProducerService.java
@@ -0,0 +1,40 @@
+package academy.devdojo.maratonajava.javacore.ZZIjdbc.service;
+
+import java.util.List;
+
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+
+public class ProducerService {
+	public static void save(Producer producer) {
+		ProducerRepository.save(producer);
+	}
+	
+	public static void delete(Integer id) {
+		requiredValidId(id);
+		ProducerRepository.delete(id);
+	}
+	
+	public static void update(Producer producer) {
+		requiredValidId(producer.getId());
+		ProducerRepository.update(producer);
+	}
+	
+	private static void requiredValidId(Integer id) {
+		if (id == null && id <= 0) {
+			throw new IllegalArgumentException("Invalid value for id");
+		}
+	}
+	
+	public static List<Producer> findAll() {
+		return ProducerRepository.findAll();
+	}
+	
+	public static List<Producer> findByName(String name) {
+		return ProducerRepository.findByName(name);
+	}
+	
+	public static void showProducerMetaData() {
+		ProducerRepository.showProducerMetaData();
+	}
+}
--- src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -1,14 +1,23 @@
 package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 
+import java.util.List;
+
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
-import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.service.ProducerService;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class ConnectionFactoryTest01 {
 	public static void main(String[] args) {
 		Producer producer = Producer.builder().name("Studio Deen").build();
-		ProducerRepository.save(producer);
+		Producer producerToUpdate = Producer.builder().id(1).name("MADHOUSE").build();
+//		ProducerService.save(producer);
+//		ProducerService.delete(4);
+//		ProducerService.update(producerToUpdate);
+//		List<Producer> producers = ProducerService.findAll();
+//		List<Producer> producers = ProducerService.findByName("Mad");
+//		log.info("Producers found '{}'", producers);
+		ProducerService.showProducerMetaData();
 //		log.info("info");
 //		log.debug("debug");
 //		log.warn("warn");
